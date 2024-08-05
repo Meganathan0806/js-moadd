07e083e3e591c3b4062c0290e61ada10aeebb151
Meganathan <bmeganathan0806@gmail.com>
2024-08-06 01:13:37 +0530

    6

---
+++ mode.js
@@ -0,0 +1,6 @@
+function adjust() {
+    var a=document.getElementById('number1').value;
+    var b=document.getElementById('number2').value;
+    document.getElementById('final').innerHTML+=parseInt(a)+parseInt(b);
+    
+}
